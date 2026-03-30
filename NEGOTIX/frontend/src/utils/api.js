e52c092467ad95e6ca4d9640d6a05ad48aee0ba6
Prathmesh Kharwade <prathmeshkharwade23@gmail.com>
2026-03-30 14:53:24 +0530

    changes

--- NEGOTIX/frontend/src/utils/api.js
+++ NEGOTIX/frontend/src/utils/api.js
@@ -3,6 +3,10 @@ import axios from "axios";
 // Base URL from .env file
 const BASE_URL = import.meta.env.VITE_API_URL;
 
+if (!BASE_URL) {
+  throw new Error("VITE_API_URL is not defined");
+}
+
 // Create axios instance
 const api = axios.create({
   baseURL: BASE_URL,
