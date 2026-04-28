2d54ae51032099ee409b1fdd264d38924bc9227d
Prathmesh Kharwade <prathmeshkharwade23@gmail.com>
2026-04-28 13:32:10 +0530

    Remove CHOPPER folder from repository

--- CHOPPER/backend/src/routes/chat.route.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { handleChat, getChatHistoryHandler } from '../controllers/chat.controller.js';
-
-const router = express.Router();
-
-router.post('/', handleChat);
-router.get('/history', getChatHistoryHandler);
-
-export default router;
